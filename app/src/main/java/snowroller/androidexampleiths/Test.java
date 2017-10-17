cc11a0ef8d0c611906cb3aeef765383f8dd292bd
Martin Blomberg <martin.blomberg@outlook.com>
2017-10-17 14:45:35 +0200

    SharedPreferences

--- app/src/main/java/snowroller/androidexampleiths/Test.java
+++ app/src/main/java/snowroller/androidexampleiths/Test.java
@@ -1,6 +1,7 @@
 package snowroller.androidexampleiths;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,12 +10,16 @@ import android.widget.TextView;
 public class Test extends AppCompatActivity {
 
     private TextView textView;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         textView = (TextView)findViewById(R.id.textView6);
+
+        sharedPreferences = getSharedPreferences("TestActivity",0);
+        textView.setText(sharedPreferences.getString("textView6", "Default Value on first run"));
     }
 
     public void onEditClick(View view){
@@ -29,6 +34,9 @@ public class Test extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if( requestCode == 1){
             textView.setText(data.getStringExtra("answer"));
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("textView6",data.getStringExtra("answer"));
+            editor.apply();  //Or commit() if return value is important.
         }
     }
 }
