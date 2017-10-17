e149d1d341b43146d47285e92cfe5f478cc7d3e4
Martin Blomberg <martin.blomberg@outlook.com>
2017-10-17 13:30:53 +0200

    Intent EditActivity

--- app/src/main/java/snowroller/androidexampleiths/EditActivity.java
+++ app/src/main/java/snowroller/androidexampleiths/EditActivity.java
@@ -1,18 +1,26 @@
 package snowroller.androidexampleiths;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        editText = (EditText) findViewById(R.id.editText2);
+        Intent intent = getIntent();
+        editText.setText(intent.getStringExtra("text"));
     }
 
     public void onDoneClick(View view){
+
 
     }
 }
--- app/src/main/java/snowroller/androidexampleiths/Test.java
+++ app/src/main/java/snowroller/androidexampleiths/Test.java
@@ -1,18 +1,27 @@
 package snowroller.androidexampleiths;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class Test extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        textView = (TextView)findViewById(R.id.textView6);
     }
 
     public void onEditClick(View view){
+        Intent intent = new Intent(this, EditActivity.class);
+        intent.putExtra("text", textView.getText().toString());
+
+        startActivity(intent);
 
     }
 }
