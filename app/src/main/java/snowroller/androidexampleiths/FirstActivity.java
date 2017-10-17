aa2721291aea9c64eba932caaa8da20b8d06baf4
Martin Blomberg <martin.blomberg@outlook.com>
2017-10-17 11:20:13 +0200

    Intents

--- app/src/main/java/snowroller/androidexampleiths/FirstActivity.java
+++ app/src/main/java/snowroller/androidexampleiths/FirstActivity.java
@@ -1,18 +1,28 @@
 package snowroller.androidexampleiths;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        editText = (EditText)findViewById(R.id.editText);
     }
 
     public void sendButton(View view){
         //Här ska vi starta aktivitet SecondActivity
+        Intent intent = new Intent(this , SecondActivity.class);
+        //Skicka med ett meddelande som en textsträng
+        intent.putExtra("Message",editText.getText().toString());
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/snowroller/androidexampleiths/SecondActivity.java
+++ app/src/main/java/snowroller/androidexampleiths/SecondActivity.java
@@ -1,13 +1,26 @@
 package snowroller.androidexampleiths;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //Hämta intentet som startade oss
+        Intent intent = getIntent();
+
+        String text = intent.getStringExtra("Message");
+        textView = (TextView)findViewById(R.id.textView5);
+        textView.setText(text);
+
+
     }
 }
